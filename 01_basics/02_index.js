1d6294c818e005cfbf4599cf7701ee9a3f4c09bd
mandeep259 <79083326+mandeep259@users.noreply.github.com>
2025-01-01 09:45:22 +0000

    variables

---
+++ 01_basics/02_index.js
@@ -0,0 +1,13 @@
+console.log("Hi");
+
+let yourName = "Deep"
+let yourAge  = 35
+const LastName = "Singh"   
+
+console.log(yourName)
+console.log(LastName)
+console.log(yourAge)
+
+console.log(typeof(yourName))
+console.log(typeof(LastName))
+console.log(typeof(yourAge))
