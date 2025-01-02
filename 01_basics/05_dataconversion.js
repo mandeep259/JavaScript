da6d59c97a409ab55ad2c595df9c849049056bbf
mandeep259 <79083326+mandeep259@users.noreply.github.com>
2025-01-02 10:30:39 +0000

    dataconverstion and operations

---
+++ 01_basics/05_dataconversion.js
@@ -0,0 +1,37 @@
+// let score = "33"
+
+// console.log(score)
+// console.log(typeof score)
+
+// let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+let score = 0
+
+let scoreInBoolen = Boolean(score)
+
+// console.log(score);
+// console.log(typeof score)
+
+console.log(scoreInBoolen);
+console.log(typeof scoreInBoolen);
+
+let userPassword = "hitesh"
+
+// console.log(userPassword);
+// console.log(typeof userPassword);
+
+// ********************** operations ****************
+
+// console.log(2+2);
+// console.log(3-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(12%9);
+
+console.log(2+2 + "js" + 33);
+console.log("36" + (2 + 2) );
+
+
+
