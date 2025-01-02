bd745a0076fd00ace441c7f6fd372190fbe8fbfb
mandeep259 <79083326+mandeep259@users.noreply.github.com>
2025-01-02 06:49:49 +0000

    variable section

---
+++ 01_basics/03_variables.js
@@ -0,0 +1,18 @@
+
+const userEmail = "mandeep@gmail.com"
+const userMobileNumber = 1234567890
+const userName = "Deep"
+let userState = "Punjab"
+var userLastName = "Singh"
+userPincode = 144526
+// userEmail ="deep@gmail.com" const valuse cann't be changed
+// double slash represent as comment and javascript engine ignore this line.
+/*
+This represent to comment the multiple line in JS. And more thing "var" is not used in the modern JS due to its block and funtion
+as it behave as inappropriate behave
+*/
+userHobbies =
+console.log(userEmail)
+
+console.group (userName, userLastName, userEmail, userMobileNumber, userEmail, userState, userPincode)
+console.log(userHobbies) // if valuse not assigned to its variableName, it represent datatype as undefined.
