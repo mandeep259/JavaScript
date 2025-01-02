18693d26e361223ae6de1351ba36c1221edbe050
mandeep259 <79083326+mandeep259@users.noreply.github.com>
2025-01-02 08:23:28 +0000

    datatype section1

---
+++ 01_basics/04_datatypes.js
@@ -0,0 +1,31 @@
+"user strict";
+let userName = "Deep"
+let userAge = 35
+let userState;
+let userLoggedIn = false
+
+// alert(3+3) this is run in a browser, not in a Node
+// console.log(userName)
+// console.log(userAge)
+
+// console.log(typeof userName)
+// console.log(typeof userAge)
+// console.log(typeof userState)
+// console.log(typeof userLoggedIn)
+/*
+Datatypes
+string
+number
+bigint
+boolean
+null
+undefined
+symbol
+*/
+// object
+
+console.log(typeof Boolean)
+console.log(typeof Number)
+console.log(typeof null)
+console.log(typeof BigInt)
+console.log(typeof Symbol)
