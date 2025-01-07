ed3193711b0d93f34d4678e564746efcca12b2f6
mandeep259 <79083326+mandeep259@users.noreply.github.com>
2025-01-07 09:54:40 +0000

    math

---
+++ 01_basics/06_num_and_maths.js
@@ -0,0 +1,18 @@
+let score = 100;
+
+// console.log(score);
+// console.log(typeof score);
+
+let scoreOne = new Number(1000);
+
+// console.log(scoreOne);
+console.log(scoreOne.toString().length);
+console.log(scoreOne.toPrecision(3));
+console.log(scoreOne.toFixed(2));
+console.log(typeof (scoreOne));
+
+console.log(typeof scoreOne.toString());
+
+
+
+
