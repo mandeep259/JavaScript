6af65639e91e34eb99e2432c6811041c28c0f93d
mandeep259 <79083326+mandeep259@users.noreply.github.com>
2025-01-07 12:04:26 +0000

    math prototpes and its function

--- 01_basics/06_num_and_maths.js
+++ 01_basics/06_num_and_maths.js
@@ -6,12 +6,31 @@ let score = 100;
 let scoreOne = new Number(1000);
 
 // console.log(scoreOne);
-console.log(scoreOne.toString().length);
-console.log(scoreOne.toPrecision(3));
-console.log(scoreOne.toFixed(2));
-console.log(typeof (scoreOne));
+// console.log(scoreOne.toString().length);
+// console.log(scoreOne.toPrecision(3));
+// console.log(scoreOne.toFixed(2));
+// console.log(typeof (scoreOne));
 
-console.log(typeof scoreOne.toString());
+// console.log(typeof scoreOne.toString());
+
+// *********************** Maths *****************
+
+// console.log(scoreOne);
+// console.log(Math.floor((Math.random() *10) + 1));
+
+const min = 5;
+const max =25; 
+
+// console.log(Math.floor(Math.random( max - min) * min) + min);
+
+// console.log(Math.floor(Math.random( max - min)* min) + min);
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));  //correct one
+
+let value = 1000000;
+
+console.log(value.toLocaleString("en-IN")); //to resent the value in indian standard numbers.
+
 
 
 
