f19af9a42a15a755eadc1e941307860cacaed11a
mandeep259 <79083326+mandeep259@users.noreply.github.com>
2025-01-08 15:56:34 +0000

    array examples

---
+++ 02_basics/02_array.js
@@ -0,0 +1,33 @@
+const marvelHeros = ["ironman", 'spiderman', 'thor']
+const dcHeros = ['flash', 'superman', 'he-man'] 
+
+// allHeros = marvelHeros + dcHeros
+
+// console.log(typeof allHeros); //it converts array into a string
+
+// marvelHeros.push(dcHeros)
+// console.log("Using push method ", marvelHeros );
+
+
+let newHeros= marvelHeros.concat(dcHeros);
+console.log(newHeros);
+
+newF = [...marvelHeros, ... dcHeros]
+console.log(newF);
+
+console.log(newHeros.includes('spiderman'))
+console.log(newHeros.includes('gentleman'))
+
+console.log(Array.isArray(newHeros))
+console.log(Array.from('mandeep'))
+console.log(Array.from({name: 'Deep'})) //return as empty object
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3))
+
+
+
