6078ac877ffd2eb4cadeb6e8353c4d94d8409487
mandeep259 <79083326+mandeep259@users.noreply.github.com>
2025-01-09 07:04:15 +0000

    array examples

---
+++ 02_basics/03_basic_array_practice.js
@@ -0,0 +1,29 @@
+
+arrOne = [11,12,13,14,15]
+
+console.log(arrOne);
+console.log(arrOne.length);
+console.log(arrOne[0])
+
+arrOne[0]= 100;
+console.log(arrOne);
+
+arrOne.push(16)
+console.log(arrOne);
+
+arrOne.pop()
+console.log(arrOne);
+
+arrOne.unshift(9)
+console.log(arrOne);
+
+arrOne.shift()
+console.log(arrOne);
+
+
+console.log(arrOne.indexOf(11));
+
+console.log(arrOne.includes(15))
+
+console.log(arrOne.join())
+console.log(arrOne.join('-'))
