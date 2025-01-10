9b8cd01f6993c9b4a371e1e67a77f323a86e91ef
mandeep259 <79083326+mandeep259@users.noreply.github.com>
2025-01-10 11:12:42 +0000

    modify e.g.

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -5,3 +5,16 @@ function username (anyname = "Jay"){
 
 username("Mohit")
 
+function addTwoNumber(num1, num2){
+    console.log(num1 + num2);
+}
+
+addTwoNumber(5,11); //if argument not given, it give o/p as NaN.
+// TolalSum = addTwoNumber(2, 3)
+// console.log(TolalSum);
+
+let multiply = function(a, b) {
+    return a*b;
+}
+
+console.log(multiply(23,5));
