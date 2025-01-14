2ee3db0a0383c592f2848b5fc9a36055cd519371
mandeep259 <79083326+mandeep259@users.noreply.github.com>
2025-01-14 10:11:05 +0000

    funtion

--- 03_basics/02_function_example.js
+++ 03_basics/02_function_example.js
@@ -4,3 +4,9 @@ function addTwoNumber (num1, num2){
 
 addTwoNumber(2,4)
 
+// expression fution
+const subTwoNumber = function(a, b){ 
+   console.log(`the sub of A and B is ${a -b}`);
+}
+
+subTwoNumber(100, 50)
