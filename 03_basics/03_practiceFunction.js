30b209496da464cc1d7fd7d0bf0478170a6f8abf
mandeep259 <79083326+mandeep259@users.noreply.github.com>
2025-01-16 06:37:34 +0000

    practice

---
+++ 03_basics/03_practiceFunction.js
@@ -0,0 +1,35 @@
+
+// console.log("D");
+// console.log("E");
+// console.log("E");
+// console.log("P");
+
+// Function: function makes repeated tasks very easily.
+
+// function sayMyName(){
+//     console.log("D");
+//     console.log("E");
+//     console.log("E");
+//     console.log("P");
+// }
+
+// sayMyName()
+
+// function addTwoNumber(val1, val2) {
+//     let result = val1 + val2
+//     return result
+// }
+
+// // console.log(addTwoNumber(5,20))
+// let TotalVal = addTwoNumber(5,35)
+// console.log("Result", TotalVal);
+// console.log(TotalVal);
+
+
+// function addTwoNumber(val1, val2) {
+//         return val1 + val2
+// }
+
+// console.log(addTwoNumber(5,"35a"))
+    
+
