c1a2b264b0fe8fe65c92257b20c1ff7beea06528
mandeep259 <79083326+mandeep259@users.noreply.github.com>
2025-01-22 11:26:28 +0000

    IIFE more expamples

--- 03_basics/07_iife.js
+++ 03_basics/07_iife.js
@@ -17,3 +17,41 @@
     console.log(`DB CONNECTED THREE by, ${name}`);
     
 } ) ("Hitesh")
+
+
+let counter = ( () => {
+    let counter =0;
+    return {
+        increment: () => {
+            counter ++
+            console.log(`Increment Counter, ${counter}`);
+            },
+        decrement: () => {
+            counter --;
+            console.log(`Decrement Counter, ${counter}`);
+        }
+    }
+} )()
+
+counter.increment();
+counter.increment();
+counter.decrement();
+// let counter = ( () => {
+//     let count = 0;
+    
+//     return {
+//       increment: () => {
+//         count++;
+//         console.log("Counter:", count);
+//       },
+//       decrement: () => {
+//         count--;
+//         console.log("Counter:", count);
+//       }
+//     };
+//   } ) ();
+  
+//   counter.increment();  // Output: Counter: 1
+//   counter.increment();  // Output: Counter: 2
+//   counter.decrement();  // Output: Counter: 1
+  
