dd78691c3a6a3a57f35fd07809661dd03b49786b
mandeep259 <79083326+mandeep259@users.noreply.github.com>
2025-01-22 11:04:52 +0000

    examples for IIFE

--- 03_basics/07_iife.js
+++ 03_basics/07_iife.js
@@ -1,1 +1,19 @@
-// IMMEDIATEDLY INVOKED FUNCTION EXECUTION (IIFE)
+// IMMEDIATEDLY INVOKED FUNCTION EXPRESSIONS (IIFE)
+
+(function chai (){
+    //named IIFE
+    console.log("DB CONNECTED");
+}) ();
+
+// chai()
+
+// () () iife syntax
+
+( () => {
+    console.log("DB CONNECTED TWO");
+    } )();
+
+( (name) => {
+    console.log(`DB CONNECTED THREE by, ${name}`);
+    
+} ) ("Hitesh")
