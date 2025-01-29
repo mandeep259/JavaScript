dd7a328dfc88dc4f1e276b6a4758b576e0df3a67
mandeep259 <79083326+mandeep259@users.noreply.github.com>
2025-01-29 06:58:31 +0000

    switch case

--- 04_control_flow/two.js
+++ 04_control_flow/two.js
@@ -1,21 +1,43 @@
-const month = "march"
-// switch conditions
+// const month = "march"
+// // switch conditions
+
+// switch (month) {
+//     case "jan":
+//         console.log("January");
+//         break;
+//     case "feb":
+//         console.log("Febuary");
+//         break;
+//     case "march":
+//         console.log("March");
+//         break;
+//     case "april":
+//         console.log("April");
+//         break;
+
+//     default:
+//         console.log("Default case match");
+//         break;
+// }
+
+const month = 3;
 
 switch (month) {
-    case "jan":
+    case 1:
         console.log("January");
         break;
-    case "feb":
+    case 2:
         console.log("Febuary");
         break;
-    case "march":
+    case 3:
         console.log("March");
-        break;
-    case "april":
+        // break;
+    case 4:
         console.log("April");
         break;
-
+    case 5:
+        console.log("May");
+        break;
     default:
-        console.log("Default case match");
         break;
 }
