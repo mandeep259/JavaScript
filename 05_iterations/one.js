47b0d9d7bb28c9d5e621d3d6071df65d363be1f3
mandeep259 <79083326+mandeep259@users.noreply.github.com>
2025-02-03 08:47:43 +0000

    iteration more examples

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -7,22 +7,31 @@
 // }
 // console.log(element);  outside the scope
 
-for (let i = 0; i <= 10; i++) {
-    const element = i;
-    if (i == 5) {
-        console.log("5 is the Best Number");
-        break;
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if (i == 5) {
+//         console.log("5 is the Best Number");
+//         break;
+//     }
+//     console.log(element);
+// }
+
+// let myArray = ['Skatiman', 'Flash', 'Superman', 'Thor']
+
+// console.log(myArray.length);
+
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+// }
+
+
+for (let i = 0; i < 10; i++) {
+    // console.log(`Outer loop value is ${i}`);
+    
+    for (let j = 0; j < 10; j++) {
+        // console.log(`INNER Loop value is ${j}`);
+        // console.log(`The INNER value is ${j} and OUTER value is ${j}`);
+        console.log(`${i} * ${j} =`, i * j);
     }
-    console.log(element);
-}
-
-let myArray = ['Skatiman', 'Flash', 'Superman', 'Thor']
-
-console.log(myArray.length);
-
-for (let index = 0; index < myArray.length; index++) {
-    const element = myArray[index];
-    console.log(element);
-    
-    
 }
