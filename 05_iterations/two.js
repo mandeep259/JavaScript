bf5105dd6c004691247f24785e87c62a0e35ea82
mandeep259 <79083326+mandeep259@users.noreply.github.com>
2025-02-03 11:36:14 +0000

    Do while examples

---
+++ 05_iterations/two.js
@@ -0,0 +1,17 @@
+// let i =0
+
+// while (i <= 10) {
+//     console.log(i);
+//     // i++ 
+//     i = i +1
+// }
+
+let score = 100
+do {
+    console.log(score);
+    score++
+    
+} while (score <= 10);
+
+
+   
